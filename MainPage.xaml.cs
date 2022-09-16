3435de62a75beefa0321031f7d6bda391d4673e7
YOVO Yawo Emmanuel <emmanuelyovo649@gmail.com>
2022-09-16 11:33:14 +0200

    implement MVVM architecture and adding Task

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace MyTasks;
+﻿using MyTasks.ViewModels;
+
+namespace MyTasks;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	public MainPage(MainViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 
 }
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTasks.ViewModels
+{
+    public partial class MainViewModel: ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+            Items.Add(text); 
+            // Adding the item intered by user
+            Text = String.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string item)
+        {
+            if (Items.Contains(item))
+            {
+                Items.Remove(item);
+            }
+        }
+    }
+}
